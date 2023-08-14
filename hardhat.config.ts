822aeb8b511fc45d163db80f29d0e50ad998ae10
Stjepan Horvat <StjepanH@teched.hr>
2023-08-15 00:15:24 +0200

    Upgradeability contracts and test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
